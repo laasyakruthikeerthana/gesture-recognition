72461fa49f4a82a5cc28761bb81b1a9ce3a00951
laasyakruthikeerthana <kbhagyasreesandhya212@gmail.com>
2026-06-03 15:30:11 +0530

    Add basic gesture recognition using OpenCV

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 print("Gesture Recognition Project")
+import cv2
+
+camera = cv2.VideoCapture(0)
+
+while True:
+ret, frame = camera.read()
+
+```
+cv2.imshow("Gesture Recognition", frame)
+
+if cv2.waitKey(1) & 0xFF == ord('q'):
+    break
+```
+
+camera.release()
+cv2.destroyAllWindows()
